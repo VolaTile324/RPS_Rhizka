65c658f4a037bd39476e7320064535ad2a4812d4
Rhizka Subentar <rhizkandro@gmail.com>
2023-04-17 17:22:41 +0700

    Added resolution options (experimental)

--- Assets/Scripts/SettingsPanelSCR.cs
+++ Assets/Scripts/SettingsPanelSCR.cs
@@ -12,6 +12,9 @@ public class SettingsPanelSCR : MonoBehaviour
     [SerializeField] Slider sfxSlider;
     [SerializeField] TMP_Text bgmVolText;
     [SerializeField] TMP_Text sfxVolText;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private TMP_Dropdown windowmodeDropdown;
+    private int currentResolutionIndex;
 
     private void OnEnable()
     {
@@ -20,6 +23,39 @@ public class SettingsPanelSCR : MonoBehaviour
         sfxSlider.value = audioManager.SFXVolume;
         SetBGMVolText(bgmSlider.value);
         SetSFXVolText(sfxSlider.value);
+
+        ResolutionEntries();
+    }
+
+    public void ResolutionEntries()
+    {
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            string option = Screen.resolutions[i].width + " x " + Screen.resolutions[i].height;
+            options.Add(option);
+            if (Screen.resolutions[i].width == Screen.currentResolution.width &&
+                               Screen.resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        currentResolutionIndex = PlayerPrefs.GetInt("Resolution", 0);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution()
+    {
+        Resolution resolution = Screen.resolutions[resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        resolutionDropdown.RefreshShownValue();
+
+        PlayerPrefs.SetInt("Resolution", resolutionDropdown.value);
+        PlayerPrefs.Save();
     }
 
     public void SetBGMVolText(float value)
